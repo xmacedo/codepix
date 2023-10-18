10196a0d1dc69f5020169c120b2278a1cced53a7
Felipe Macedo <felipe.macedo@capitanigroup.com>
2023-10-18 12:01:09 -0300

    Aula 1

---
+++ domain/model/pixKey.go
@@ -0,0 +1,55 @@
+package model
+
+type PixKeyRepositoryInterface interface {
+	Register(pixKey *PixKey) (*PixKey, error)
+	FindKeyByKind(key string, kind string) (*PixKey, error)
+	AddBank(bank *Bank) error
+	AddAccount(account *Account) error
+	FindAccount(id string) (*Account, error)
+}
+
+type PixKey struct{
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       *Bank    `json:"key" valid:"notnull"`
+	AccountID string   `json:"account_id" valid:"notnull"`
+	Account   *Account `valid: "-"`
+	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+
+	if pixKey.Kind != "email" && pixKey.Kind != "cpf" {
+		return errors.New("invalid type of key")
+	}
+
+	if pixKey.Status != "active" && pixKey.Status != "inactive" {
+		return errors.New("invalid status")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
+
+	pixKey := PixKey{
+		Kind:    kind,
+		Key:     key,
+		Account: account,
+		Status:  "active",
+	}
+
+	pixKey.ID = uuid.NewV4().String()
+	pixKey.CreatedAt = time.Now()
+
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pixKey, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xmacedo/codepix-go
+
+go 1.15
